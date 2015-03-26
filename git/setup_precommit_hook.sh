634b65f8c7dbb97d4c00f21547df67b8159a074e
Vincent <suanmeiguo@gmail.com>
2015-03-25 20:14:14 -0700

    fix bug on wrong soft link path

--- git/setup_precommit_hook.sh
+++ git/setup_precommit_hook.sh
@@ -7,4 +7,4 @@ SRC=code-quality/git
 sudo pip install -r ${SRC}/requirements.txt
 
 # setup precommit hook by linking the pre-commit script to the git hooks directory
-cd ${GIT_ROOT}/.git/hooks/ && ln -s $(pwd)/${SRC}/pre-commit.py pre-commit
+cd ${GIT_ROOT} && ln -s $(pwd)/${SRC}/pre-commit.py .git/hooks/pre-commit
